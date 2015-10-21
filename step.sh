d5f50a388bcd24fb41c59af20cb45bf4a8534c7d
Krisztian Godrei <godrekrisztian@gmil.com>
2015-10-21 18:34:00 +0100

    install nunit console

--- step.sh
+++ step.sh
@@ -11,6 +11,29 @@ if [ ! -z "${workdir}" ] ; then
 	cd "${workdir}"
 fi
 
+echo
+echo "==> Install nunit console"
+echo
+# Download nunit-console
+NUNIT_DOWNLOAD_LINK="http://github.com/nunit/nunitv2/releases/download/2.6.4/NUnit-2.6.4.zip"
+DOWNLOAD_DIR="${HOME}/.nunit"
+DOWNLOAD_LOCATION="${DOWNLOAD_DIR}/NUnit-2.6.4.zip"
+
+rm -rf "${DOWNLOAD_DIR}"
+mkdir -p "${DOWNLOAD_DIR}"
+curl -fL "${NUNIT_DOWNLOAD_LINK}" > "${DOWNLOAD_LOCATION}"
+
+# Unzip nunit-console
+UNZIP_LOCATION="${HOME}/.nunit"
+unzip "${DOWNLOAD_LOCATION}" -d "${DOWNLOAD_DIR}"
+rm -rf "${DOWNLOAD_LOCATION}"
+
+# Export nunit-console path
+NUNIT_CONSOLE="${UNZIP_LOCATION}/NUnit-2.6.4/bin/nunit-console.exe"
+
+echo
+echo "==> Performing step"
+echo
 ruby "${THIS_SCRIPTDIR}/step.rb" \
 	-s "${xamarin_solution}" \
 	-c "${xamarin_configuration}" \
@@ -18,4 +41,4 @@ ruby "${THIS_SCRIPTDIR}/step.rb" \
 	-b "${xamarin_builder}" \
 	-d "${simulator_device}" \
 	-o "${simulator_os_version}" \
-	-n "${nunit_console_path}"
+	-n "${NUNIT_CONSOLE}"
